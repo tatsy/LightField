1ee2759374f55691ec1c47ee3d7219b2cd626a11
tatsy <tatsy.mail@gmail.com>
2015-12-08 15:32:51 +0900

    Update Qt detection script.

--- viewer/directories.h
+++ viewer/directories.h
@@ -7,6 +7,6 @@
 
 #include <string>
 
-const std::string kShaderDirectory = "C:/Users/Tatsuya/Desktop/LightField/shaders/";
+const std::string kShaderDirectory = "C:/Users/Tatsuya/Documents/Programs/Cpp/LightField/shaders/";
 
 #endif  // _DIRECTORYES_H_
